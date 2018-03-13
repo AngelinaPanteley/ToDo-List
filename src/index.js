67650fd68027ebb1507dc813d6e30ff1493bdf79
AngelinaPanteley <panteley-00@mail.ru>
2018-03-13 17:51:07 +0300

    basic structure

--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import { render } from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import App from './components/App'
+import reducer from './data/reducer'
+import { Route, Switch } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import './styles/index.css'
+import './styles/index.css'
+
+export const initialState = {
+  categories: {  categoryList: [], progress: 0 },
+  tasks: {taskList: []},
+  filters: {showDone: false, search: ""}
+}
+
+export const store = createStore(
+  reducer, initialState, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
+render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component={Dashboard} />
+      </Switch>
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root')
+)
